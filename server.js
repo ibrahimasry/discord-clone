6f8bfeda7a71196b09d0ac853c29675668f487ff
ibrahim masry <ibrahim.elmansoury@gmail.com>
2023-03-16 11:42:50 +0200

    dep

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const socketServer = require("./socketServer");
+const authRoutes = require("./routes/authRoutes");
+const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
+const { fstatSync } = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(
+  express.static(
+    path.join(path.resolve(), "discord-frontend", "build", "index.html")
+  )
+);
+// register the routes
+app.use("/api/auth", authRoutes);
+app.use("/api/friend-invitation", friendInvitationRoutes);
+app.get("/*", (req, res) => {
+  return res.sendFile(
+    path.join(path.resolve(), "discord-backend", "build", "index.html")
+  );
+});
+const server = http.createServer(app);
+socketServer.registerSocketServer(server);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("database connection failed. Server not started");
+    console.error(err);
+  });
